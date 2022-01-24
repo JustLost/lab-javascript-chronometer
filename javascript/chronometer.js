a59fa1ba13273551eac3e4c233166f1584697abe
Rita Cunha <rita.cunha.ts@gmail.com>
2022-01-24 18:14:56 +0000

    Lab finished

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,48 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(callback) {
-    // ... your code goes here
+    // ... your code goes here    
+    this.intervalId = setInterval(() => {
+      this.currentTime++; 
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
-  }
+     //... your code goes here
+    if( this.currentTime < 60 ) {return 0}
 
+    return Math.floor(this.currentTime / 60)    
+  }
+  
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    if(value < 10) return `0${value}`
+    if(value > 10) return `${value}`
+
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
     // ... your code goes here
+    return `${this.computeTwoDigitNumber(this.getMinutes(this.currentTime))}:${this.computeTwoDigitNumber(this.getSeconds(this.currentTime))}`;
   }
 }
 
